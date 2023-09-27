f8d039fe081a3de79f6701d70c1de9ddb93ea4b0
TylerOppy <78933324+TylerOppy@users.noreply.github.com>
2023-09-27 13:21:53 -0400

    Create Lab5.py

---
+++ labs/Lab5.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+# Input: Birthday date in the format 'YYYY-MM-DD'
+birthday_str = input("Enter your birthday (YYYY-MM-DD): ")
+
+# Convert the input string to a datetime object
+try:
+    birthday_date = datetime.strptime(birthday_str, "%Y-%m-%d")
+except ValueError:
+    print("Invalid date format. Please use the format 'YYYY-MM-DD'")
+else:
+    # Calculate the age in seconds
+    current_date = datetime.now()
+    age_in_seconds = (current_date - birthday_date).total_seconds()
+
+    print(f"You are approximately {int(age_in_seconds)} seconds old.")
