4510b8569f16922ec5aede958ceaaa05e446e5b5
TylerOppy <78933324+TylerOppy@users.noreply.github.com>
2023-10-04 09:23:42 -0400

    Update test.js

--- node/test.js
+++ node/test.js
@@ -3,3 +3,15 @@ $ node test.js
 “Hello, World”
 var hello = "Hello from Node JS Variable!"
 `console.log(hello)`
+
+console.log (`Printing variable hello: ${hello}`);
+
+console.log("directory name: " + __dirname); 
+console.log("directory and file name: " + __filename); 
+
+var path = require("path");
+
+console.log("Using PATH module:");
+console.log(`Hello from file ${path.basename(__filename)}`);
+
+console.log(`Process args: ${process.argv}`)
